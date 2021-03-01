07180e889539c7070a9abeab3f692311266b0e02
Tim M <timothy.romero.manas@gmail.com>
2021-02-28 21:49:10 -0500

    Creating Base Application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import { BrowserRouter, Route } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen.js'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
