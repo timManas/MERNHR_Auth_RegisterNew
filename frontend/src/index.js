d9244837f8c403958b38bba4fe86ef4ae83df333
Tim M <timothy.romero.manas@gmail.com>
2021-03-02 21:57:47 -0500

    Add home page elements

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import './bootstrap.min.css'
 import { Provider } from 'react-redux'
+import store from './store.js'
 
 ReactDOM.render(
   <Provider store={store}>
