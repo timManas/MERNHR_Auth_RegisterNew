ac976cf4bb426ac8b77a79f6b1a6f6fb2664a488
Tim M <timothy.romero.manas@gmail.com>
2021-03-04 20:31:29 -0500

    Added backend for Registering new user

--- backend/userController.js
+++ backend/userController.js
@@ -2,6 +2,9 @@ import asyncHandler from 'express-async-handler'
 import User from './UserModel.js'
 import generateToken from './generateToken.js'
 
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
 export const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
 
@@ -20,6 +23,9 @@ export const authUser = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc    Get User Profile
+// @route   GET /api/users/profile
+// @access  Private
 export const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id)
   console.log('user: ' + req.user)
@@ -34,5 +40,38 @@ export const getUserProfile = asyncHandler(async (req, res) => {
   } else {
     res.status(404)
     res.json({ error: 'User not found' })
+  }
+})
+
+// @desc    Register new user
+// @route   POST /api/users/
+// @access  Public
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+  if (userExists) {
+    res.status(400) // Bad reqyest
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(201)
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
   }
 })
--- backend/userModel.js
+++ backend/userModel.js
@@ -35,7 +35,7 @@ userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
     next()
   }
-  const salt = await bcrypt.genSalt(10)
+  const salt = await bcrypt.genSalt(10) // need salt to encrypt the password
   this.password = await bcrypt.hash(this.password, salt) // Hashed password
 })
 
